35e722442290576d99617837509d887ec11d046b
pchavan <pavan.chavan@forescout.com>
2025-03-02 13:32:58 +0530

    update loader data

--- pages/bajarbhav/[view]/index.js
+++ pages/bajarbhav/[view]/index.js
@@ -33,7 +33,7 @@ export async function getServerSideProps({ params }) {
 
 const MarketType = ({ view, meta, marketTypes }) => {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const handleStart = () => setIsLoading(true);
